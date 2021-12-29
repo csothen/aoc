1cd39f50db291af6daf70c9136b63930d06ec273
csothen <cesarjcpinheiro@gmail.com>
2021-12-29 19:06:20 +0000

    2015 day 4

--- go/2015/day4.go
+++ go/2015/day4.go
@@ -8,8 +8,17 @@ import (
 
 func Day4() {
 	base, hash := "ckczppom", ""
+	// part 1
+	hash("00000")
+
+	// part 2
+	hash("000000")
+}
+
+func hash(prefix string) {
+	base, hash := "ckczppom", ""
 	i := -1
-	for !strings.HasPrefix(hash, "000000") {
+	for !strings.HasPrefix(hash, prefix) {
 		i++
 		input := fmt.Sprintf("%s%d", base, i)
 		hash = fmt.Sprintf("%x", md5.Sum([]byte(input)))
